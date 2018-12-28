661e1ee252da749049afbe8b407669b3d0c13f32
cwsehn <cwsehn@gmail.com>
2018-12-28 17:19:50 -0600

    Asynchronous Network Callback functions Error Handling JSONDownloader class DarkSkyError enum

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -18,9 +18,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
+    // https://darksky.net/dev/account
+    fileprivate let darkSkyAPIKey = "469a2e386f9f712bbc0c2d8ce2ecac51"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        let baseURL = URL(string: "https://api.darksky.net/forecast/\(darkSkyAPIKey)/")
+        guard let forecastURL = URL(string: "37.8267,-122.4233", relativeTo: baseURL) else {
+            return
+        }
+        
+        /*
+         //This represents the synchronous method for fetching a Data Object as JSON...
+        let weatherData = try? Data(contentsOf: forecastURL!)
+        print(weatherData ?? "NoData")""
+        let json = try? JSONSerialization.jsonObject(with: weatherData!, options: [])
+        print(json ?? "NoJSON")
+        */
+        
+        // a URLRequest object creates an HTTP GET request by default....
+        let request = URLRequest(url: forecastURL)
+        // a URLSessionConfiguration object allows for multiple settings including authentication and cache...
+        //... the Configuration is used to initialize the Session...this one uses default settings...
+        let configuration = URLSessionConfiguration.default
+        // a URLSession object allows for asynchronous Data fetching...
+        let session = URLSession(configuration: configuration)
+        // the Session.dataTask() method returns data directly to the app as a Data object...using closure parameters...
+        // by contrast .downloadTask() retrieves contents of request and saves to temp file on disk...
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            print(data ?? "Task failed")
+        }
+        
+        // resume method calls session.dataTask()...
+        dataTask.resume()
+
         let currentWeather = CurrentWeather(temperature: 90.0, humidity: 0.8, precipProbability: 0.2, summary: "Hot", icon: "clear-night")
         let viewModel = CurrentWeatherViewModel(model: currentWeather)
         displayWeather(using: viewModel)
